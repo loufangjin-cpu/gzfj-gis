7796a7b4e6b8775d4bdbf11f4cee77dd16fb9078
loufangjin <18892302110@163.com>
2023-08-15 17:29:01 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'standard',
+  // 添加了运行环境设定，设置 browser 为 true
+  env: {
+    browser: true
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+class InitGisApi {
+  constructor (name) {
+    this.name = name
+  }
+}
+export default InitGisApi
